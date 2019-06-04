5a32282e0d8d40882bb3621b0723648856bba1b5
Christopha-Zhu <514648862@qq.com>
2019-06-04 09:07:51 +0800

    8.1

--- ch02 学习基础与线性模型/3.MBGD.py
+++ ch02 学习基础与线性模型/3.MBGD.py
@@ -4,6 +4,10 @@
 # 当b=1时，小批量随机下降法就等价与SGD；当b=N时，小批量就等价于全批量。所以小批量梯度下降法的效果也和b的选择相关，这个数值被称为批量尺寸(batch size)。
 # 如何确定b 参见 https://arxiv.org/abs/1609.04836
 
+
+import pandas as pd
+import numpy as np
+
 def compute_grad_batch(beta, batch_size, x, y):
     grad = [0, 0]
     r = np.random.choice(range(len(x)), batch_size, replace=False)
